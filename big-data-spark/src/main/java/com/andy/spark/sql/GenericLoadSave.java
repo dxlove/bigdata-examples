60c7ac4457135212c7e4deb1e3a9b085df1c3cbe
leone <exklin@gmail.com>
2019-03-31 19:06:20 +0800

    :sparkles:(add) update

--- big-data-spark/src/main/java/com/andy/spark/sql/GenericLoadSave.java
+++ big-data-spark/src/main/java/com/andy/spark/sql/GenericLoadSave.java
@@ -1,8 +1,8 @@
 package com.andy.spark.sql;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
@@ -15,24 +15,25 @@ import org.apache.spark.sql.SQLContext;
  **/
 public class GenericLoadSave {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AnalysisException {
 
         // 创建sparkContext
         SparkConf conf = new SparkConf().setAppName("rdd").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
         SQLContext sqlContext = new SQLContext(sc);
 
-        Dataset<Row> dataset = sqlContext.read().load("file:///E:\\tmp\\input\\parquet");
+        Dataset<Row> dataset = sqlContext.read().load("file:///D:/root/logs/parquet/");
+
+        dataset.createTempView("t_user");
+
+//        sqlContext.sql("select * from t_user where user_id < 100000 order by user_id desc").show(50);
+//        sqlContext.sql("select user_id, count(user_id) count from t_user group by user_id having count > 1").show();
+//        sqlContext.sql("select count(1) from t_user").show();
+        sqlContext.sql("select * from t_user where deleted = false and age < 18 and create_time < '2015-01-01 00:00:00' order by user_id").show();
 
         dataset.printSchema();
 
-        dataset.show();
-
-
-
-
-
-
+        // dataset.show();
     }
 
 }
