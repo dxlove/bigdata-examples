a2f182ee728dba012aad903e75ff3ef3a3e67359
leone <exklin@gmail.com>
2019-04-25 17:15:43 +0800

    update

--- big-data-spark/src/main/java/com/leone/spark/core/examples/nginxAccessLog/AccessLogMain.java
+++ big-data-spark/src/main/java/com/leone/spark/core/examples/nginxAccessLog/AccessLogMain.java
@@ -1,12 +1,15 @@
 package com.leone.spark.core.examples.nginxAccessLog;
 
+import com.leone.spark.utils.CommonUtil;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import java.util.Date;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,7 +20,7 @@ import java.util.regex.Pattern;
  * @author leone
  * @since 2019-04-24
  **/
-public class AccessLog {
+public class AccessLogMain {
 
     public static void main(String[] args) {
         SparkSession spark = SparkSession.builder().appName("").master("local[*]").getOrCreate();
@@ -49,16 +52,30 @@ public class AccessLog {
 
         dataFrame.registerTempTable("t_access_log");
 
-        Dataset<Row> data = spark.sql("select remote_addr, time_local, request, status, body_bytes_sent, http_referer, http_user_agent from t_access_log");
+        Dataset<Row> data = spark.sql("select remote_addr, time_local, request, status, body_bytes_sent, http_user_agent from t_access_log");
 
-        data.foreach(e -> {
+        JavaRDD<Row> rowJavaRDD = data.javaRDD();
 
-
+        rowJavaRDD.foreach(new VoidFunction<Row>() {
+            @Override
+            public void call(Row row) throws Exception {
+                String address = CommonUtil.getIpLocation(row.getString(1));
+                String[] requestArr = row.getString(3).split(" ");
+                String date = CommonUtil.dateFormat(row.getString(2));
+                LogBeanPO logBeanPO = new LogBeanPO(row.getString(1), address, date, row.getString(3), null, null, null, row.getString(4), row.getString(5), row.getString(6));
+                if (requestArr.length == 3) {
+                    logBeanPO.setReq_method(requestArr[0]);
+                    logBeanPO.setPath(requestArr[1]);
+                    logBeanPO.setHttp_deal(requestArr[2]);
+                }
+                System.out.println(logBeanPO.toString());
+            }
         });
 
-        data.write().jdbc(url, "t_access_log", props);
 
-        System.out.println(dataFrame.count());
+        // data.write().jdbc(url, "t_access_log", props);
+
+        // System.out.println(dataFrame.count());
 
         spark.stop();
     }
--- big-data-spark/src/main/java/com/leone/spark/utils/CommonUtil.java
+++ big-data-spark/src/main/java/com/leone/spark/utils/CommonUtil.java
@@ -2,10 +2,17 @@ package com.leone.spark.utils;
 
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.model.CityResponse;
-import com.maxmind.geoip2.record.*;
+import com.maxmind.geoip2.record.City;
+import com.maxmind.geoip2.record.Country;
+import com.maxmind.geoip2.record.Location;
+import com.maxmind.geoip2.record.Subdivision;
 
 import java.io.File;
 import java.net.InetAddress;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Locale;
 
 /**
  * <p>
@@ -17,6 +24,12 @@ public class CommonUtil {
 
     private CommonUtil() {
     }
+
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy:hh:mm:ss Z", Locale.ENGLISH);
+
+    private static SimpleDateFormat localTimeSdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
 
     /**
      * 获取ip地址的详细信息
@@ -58,7 +71,27 @@ public class CommonUtil {
     }
 
     public static void main(String[] args) {
+        String log = "183.238.59.97,,-,,25/Apr/2019:09:52:43 +0800,,GET / HTTP/1.1,,304,,0,,-,,Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36,,-,,-,,1";
+        System.out.println(Arrays.toString(log.split(",,")));
+        System.out.println(log.split(",,").length);
+
         System.out.println(getIpLocation("120.197.48.146"));
+
+        System.out.println(dateFormat("25/Apr/2019:09:52:43 +0800"));
+    }
+
+    /**
+     * @param str
+     *
+     * @return
+     */
+    public static String dateFormat(String str) {
+        try {
+            return localTimeSdf.format(sdf.parse(str));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
